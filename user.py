680605d525a519ddbec0e1acfca6eb6d455648db
Lee Witek <neomech@gmail.com>
2018-03-02 01:31:57 +0000

    add led turn on/off

--- user.py
+++ user.py
@@ -1,11 +1,19 @@
 import nbwebserver
 import utime
+import machine
 
 server = nbwebserver.WebServer()
+
+led = machine.Pin(22, machine.Pin.OUT)
+led.value(1)
 
 
 def lightsHandler(req, resp):
     print("lights handler", req.query)
+    if req.query == "command=on":
+        led.value(0)
+    elif req.query == "command=off":
+        led.value(1)
     resp.sendResponse(200)
 
 
